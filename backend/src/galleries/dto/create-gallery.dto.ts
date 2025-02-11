d77b7c8d12286e6405dc7e1b32f6484a35e33a64
wagJoker <137532506+wagJoker@users.noreply.github.com>
2025-02-11 06:11:48 +0200

    Delete backend/src/galleries directory

--- backend/src/galleries/dto/create-gallery.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateGalleryDto {
-  readonly name: string;
-  readonly description: string;
-  readonly location: string;
-  readonly establishedYear: number;
-}
--- backend/src/galleries/galleries.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GalleriesController } from './galleries.controller';
-import { GalleriesService } from './galleries.service';
-
-@Module({
-  controllers: [GalleriesController],
-  providers: [GalleriesService],
-})
-export class GalleriesModule {}
