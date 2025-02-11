1605b639ec6c7404fd24a160bf6ba4a33c534355
wagJoker <137532506+wagJoker@users.noreply.github.com>
2025-02-11 06:12:45 +0200

    Delete backend directory

--- backend/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GalleriesModule } from './galleries/galleries.module';
-import { ArtistsModule } from './artists/artists.module';
-
-@Module({
-  imports: [GalleriesModule, ArtistsModule],
-})
-export class AppModule {}
